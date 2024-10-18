17a1ff25194aa09360bb808555d5666dc1bb7b2d
Arela-Cristina <instagrandine@gmail.com>
2024-10-18 16:43:01 +0200

    Add: Add: pseudocode reasoning

---
+++ palindroma/main.js
@@ -0,0 +1,24 @@
+//Chiedere all’utente di inserire una parola 
+//Creare una funzione per capire se la parola inserita è palindroma
+
+//Chiediamo al utente di inserire una parola atraverso un prompt
+//Salvare il risultato in una variabile
+
+//Dichiariammo una funzione che ci ritornera un booleano che ci dica  se la parola e palindroma oppure no
+
+//Dichiariammo una variabile con una stringa vuota, ci servira dopo
+
+//facciamo un ciclo for, per ciclare ogni lettera della parola
+
+//salviamo in una variabile tutte le iterazzioni, uttilizziamo il metodo  .at() in maniera negativa per farci ritornare ogni indice che corrisponde ad ogni lettera della parola 
+
+//richiammiamo la variabile con la stringa vuota per concatennare ogni lettera ciclata e falra diventare una stringa.
+
+//fine ciclo for
+
+//console.log del risultato del valore aggiornato della variabile con la stringa vuota
+
+//condizioni
+//Se la variabile e uguale al valor del promt del utente, significa che la stringa e vuota. return true 
+
+//se no Return false
