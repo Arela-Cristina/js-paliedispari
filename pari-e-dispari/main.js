1709e95e34230d685377c964a17937c516b6c782
Arela-Cristina <instagrandine@gmail.com>
2024-10-18 19:27:55 +0200

    declare a function

--- pari-e-dispari/main.js
+++ pari-e-dispari/main.js
@@ -2,6 +2,10 @@
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
 //generiamo una funzione
+function randomNum(){
+    return Math.floor(Math.random() * 5) + 1
+  }
+  
 //utente sceglie 'par' o 'dispari'
 //assegniamo un numero random al pc con la nostra funzione creata
 //somiamo entrambi i numeri
